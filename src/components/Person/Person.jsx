2b398ec154b2c4bb99b261c596cb34bfe8077b7b
GM2022-stack <guilhermemls20@gmail.com>
2025-09-04 10:10:15 -0300

    feat: implementa componente Person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,8 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <p>Idade: {person.age}</p>
+    </div>
+  );
+};
